358cb03fa4f66a6a8df1df3b9dd3ca9c32b1e207
Endri Domi <endri.domi@studio.unibo.it>
2021-08-11 14:59:24 +0200

    add ex

---
+++ Laboratorio/Programmazione-concorrente/es0_premortem/es0_premortem.c
@@ -0,0 +1,15 @@
+#ifndef _THREAD_SAFE
+#define _THREAD_SAFE
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int main(void) {
+
+    printf("Ciao");
+
+    return 0;
+}
