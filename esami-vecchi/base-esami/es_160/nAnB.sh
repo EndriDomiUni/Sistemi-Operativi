a451afe553b7bb20658a8ab0998dacd08f1c6def
Endri Domi <endri.domi@studio.unibo.it>
2021-08-24 19:56:02 +0200

    Update nAnB.sh

--- esami-vecchi/base-esami/es_160/nAnB.sh
+++ esami-vecchi/base-esami/es_160/nAnB.sh
@@ -31,3 +31,27 @@ while (( ${N_LETTI} < ${LEN_STR} )) ; do
         exit 1
     fi
 done
+
+# cerco il resto delle B
+while (( ${NLETTI} < ${LUNGHEZZASTRINGA} )) ; do # ci sono altri caratteri da leggere
+	# prendo il carattere in posizione NLETTI (all'inizio NLETTI 0)
+	CAR=${STRINGA:${NLETTI}:1}
+	(( NLETTI=${NLETTI}+1 ))
+	if [[ ${CAR} == "B" ]] ; then 
+		((NB=$NB+1)) ;
+	else
+		# arrivo qui solo se ho letto un carattere diverso da B
+		# quindi termino senza avere verificato la simmetria
+		echo falso caso 2
+		exit 2
+	fi
+done
+
+# verifico di avere letto tante B quante A
+if [[ $NA -gt 0 && $NA == $NB ]] ; then
+	echo vero  N=$NA
+	exit 0
+else
+	echo falso caso 3
+	exit 3
+fi
