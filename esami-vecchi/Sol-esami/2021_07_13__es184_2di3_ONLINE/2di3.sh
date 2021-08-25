c222fe7f710f865bfa6ea8f03bcb456ab90482b2
Endri Domi <endri.domi@studio.unibo.it>
2021-08-25 11:00:15 +0200

    add sol

---
+++ esami-vecchi/Sol-esami/2021_07_13__es184_2di3_ONLINE/2di3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while read uno due tre quattro ; do
+	if [[ $tre != "" ]] ; then
+		car=${tre:1:1}
+		if [[ $car != "" ]] ; then
+			echo $car
+		fi
+	fi
+done < /usr/include/stdio.h
+
